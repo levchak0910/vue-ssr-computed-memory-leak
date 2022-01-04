ff309e454ba62d4ba6c5510450bc8925a84906a2
Sándor Levcsák <levchak.sanek@gmail.com>
2022-01-04 20:27:58 +0200

    implement vite computed logic

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
     <h1>Hello world</h1>
+    <Post 
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+
+import Post from "./Post.vue";
+
+import p from "../../db/posts.json"
 
 export default defineComponent({
   name: "App",
+  components: {
+    Post,
+  },
+  setup(){
+    const posts = ref(p)
+
+    return {
+      posts,
+    }
+  },
 });
 </script>
 
---
+++ vite/src/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <mark v-if="post.userId === 1">1</mark> -->
+    <mark v-if="isFirstUser">1</mark>
+    <span v-else>{{post.userId}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  name: "Post",
+  props: {
+    post: Object,
+  },
+  setup(props) {
+    const isFirstUser = computed(() => props.post.userId === 1)
+
+    return {
+      isFirstUser,
+    }
+  }
+});
+</script>
