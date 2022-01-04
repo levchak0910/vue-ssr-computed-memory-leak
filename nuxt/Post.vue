e9273b231d9fdc57b3e378cd0753764a892659d0
Sándor Levcsák <levchak.sanek@gmail.com>
2022-01-04 19:50:16 +0200

    implement nuxt computed logic

---
+++ nuxt/Post.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- <mark v-if="post.userId === 1">1</mark> -->
+    <mark v-if="isFirstUser">1</mark>
+    <span v-else>{{post.userId}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineNuxtComponent } from "#app";
+import { computed } from "#imports";
+
+export default defineNuxtComponent({
+  name: "Post",
+  props: {
+    post: Object,
+  },
+  setup(props) {
+    const isFirstUser = computed(() => props.post.userId === 1)
+
+    return {
+      isFirstUser,
+    }
+  }
+});
+</script>
+
--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
     <h1>Hello world</h1>
+    <Post 
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+    />
   </div>
 </template>
+
+<script lang="ts">
+import { defineNuxtComponent } from "#app";
+import { ref } from "#imports"
+
+import Post from "./Post.vue";
+
+import p from "../db/posts.json"
+
+export default defineNuxtComponent({
+  name: "App",
+  components: {
+    Post,
+  },
+  setup(){
+    const posts = ref(p)
+
+    return {
+      posts,
+    }
+  },
+});
+</script>
+
