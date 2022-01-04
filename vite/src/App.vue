07cd2026ce62687c8768253b778e87a2197b4538
Sándor Levcsák <levchak.sanek@gmail.com>
2022-01-04 20:27:07 +0200

    init vite

---
+++ vite/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+
+
